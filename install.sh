9fc2f44e1e95d27e01f1e2e6ca0bc876925c6f47
Ubuntu <ubuntu@ip-172-31-18-26.us-west-2.compute.internal>
2024-04-11 09:10:54 +0000

    first

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt-get update -y 
+sudo apt-get install -y autoconf 
+sudo apt-get install -y bison 
+sudo apt-get install -y build-essential 
+sudo apt-get install -y libssl-dev 
+sudo apt-get install -y libyaml-dev 
+sudo apt-get install -y libreadline6-dev 
+sudo apt-get install -y zlib1g-dev 
+sudo apt-get install -y libncurses5-dev 
+sudo apt-get install -y libffi-dev 
+sudo apt-get install -y libgdbm-dev 
+sudo apt-get install -y libdb-dev 
+sudo apt-get install -y git 
+sudo apt-get install -y curl
+sudo apt-get install -y default-libmysqlclient-dev
+sudo apt-get install -y default-mysql-client
+
+
